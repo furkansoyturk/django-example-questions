72f832ae99a8a1a79230d3c92524c97bc91b9895
furkansoyturk <furkansoyturk@icloud.com>
2022-02-06 22:14:20 +0300

    Initial commit

---
+++ question1/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# Create your models here.
+class Vehicle(models.Model):
+    id = models.AutoField(primary_key=True)
+    plate = models.CharField(max_length=10)
+
+    @classmethod
+    def get_new(cls):
+        return cls.objects.create().id
+
+
+class NavigationRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    vehicle = models.ForeignKey(Vehicle, default=Vehicle.get_new, on_delete=models.CASCADE)
+    # vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
